5681407cf9e76f1c1d8c050f884882b9f576d0f0
Richard Yance <yance0022@gmail.com>
2018-11-08 19:29:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+    attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade].push(student_name)
+  end
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+  def sort
+    sorted_hash = {}
+    @roster.each do |grade, student_names|
+      sorted_hash[grade] = student_names.sort
+    end
+    sorted_hash
+  end
+end
